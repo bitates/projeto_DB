608532277c02f02d5a73689b8c1f9b57f580bc6a
bitates <guilhermebrgoncalves@gmail.com>
2025-11-27 12:28:10 +0000

    Initialize Flask app with basic setup

---
+++ app.py
@@ -0,0 +1,12 @@
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+from flask import render_template, Flask
+import logging
+import db
+
+APP = Flask(__name__)
+
+# Start page
+@APP.route('/')
+def index():
+    stats
